4c60c5d731f032c23b4623c23921e1837190ceed
beila <beila@kaist.ac.kr>
2014-08-22 09:51:36 +0900

    Data Access Object

---
+++ src/model/LoginDao.java
@@ -0,0 +1,15 @@
+package model;
+
+public class LoginDao {
+
+	static final String ID = "longlee";
+	static final String PASSWD = "6789";
+	
+	public LoginDao() {
+		
+	}
+	
+	public boolean checkLogin(LoginDo loginDo) {
+		return ID.equals(loginDo.getId()) && PASSWD.equals(loginDo.getPasswd());
+	}
+}
