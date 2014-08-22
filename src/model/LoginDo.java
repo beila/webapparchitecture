934965dc2d1e34c1c13ae54bfd5f08d8a63d3aff
beila <beila@kaist.ac.kr>
2014-08-22 09:46:19 +0900

    Data Object

---
+++ src/model/LoginDo.java
@@ -0,0 +1,28 @@
+package model;
+
+public class LoginDo {
+
+	private String id;
+	private String passwd;
+	
+	public LoginDo() {
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+}
