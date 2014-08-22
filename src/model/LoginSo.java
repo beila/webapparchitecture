bc04e8bec7bd9a6a338b582c3792a9e08e5ff0f2
beila <beila@kaist.ac.kr>
2014-08-22 09:58:25 +0900

    Service Object

---
+++ src/model/LoginSo.java
@@ -0,0 +1,14 @@
+package model;
+
+public class LoginSo {
+
+	private LoginDao loginDao;
+	
+	public LoginSo() {
+		loginDao = new LoginDao();
+	}
+	
+	public boolean confirmLogin(LoginDo loginDo) {
+		return null != loginDo.getId() && null != loginDo.getPasswd() && loginDao.checkLogin(loginDo);
+	}
+}
